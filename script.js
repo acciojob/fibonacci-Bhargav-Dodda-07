55e668931d4bc8131b1b25da93844d70d66888b5
DODDA BHARGAV <128250258+Bhargav-Dodda-07@users.noreply.github.com>
2023-09-08 20:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function fibonacci(num) {
 // your code here
+
+	if(num <= 0){
+		return [];
+	}
+	else if(num == 1){
+		return [0];
+	}
+	else if(n == 2){
+		return [0 , 1]
+	}
+	else{
+		 const fibSequence = [0, 1];
+    for (let i = 2; i < num; i++) {
+      const nextValue = fibSequence[i - 1] + fibSequence[i - 2];
+      fibSequence.push(nextValue);
+	}
+		return fibSequence;
+}
+
 }
 
 module.exports = fibonacci;
